8f24fcce20717a5eef6e7f30b063bfb75deabe24
Yana Bochkareva <yana.boc@yandex.ru>
2022-03-09 19:33:51 +0400

    fix/added const

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let date = new Date(2021, 10, 2);
-let day = date.getDay();
-let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-let months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
-let month = date.getMonth() - 1;
-let year = date.getFullYear();
-let weekNumber = Math.ceil(date.getDate()/7);
+const day = date.getDay();
+const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+const month = date.getMonth() - 1;
+const year = date.getFullYear();
+const weekNumber = Math.ceil(date.getDate()/7);
 
 
 function getDayInfo(date) {
